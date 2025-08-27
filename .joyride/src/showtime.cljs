e8a381f21806d7277cd802b331b28db73169b97e
PEZ <pez@pezius.com>
2025-08-27 10:11:56 +0200

    Refactored transitions

--- .joyride/src/showtime.cljs
+++ .joyride/src/showtime.cljs
@@ -69,34 +69,51 @@
    :timer/last-display "00:00"})
 
 (defn timer-transition
-  "Handle timer state transitions based on action and current state"
+  "Handle timer state transitions"
   [timer-state action now-ms]
-  (let [timer-type (:timer/type timer-state)]
-    (case [(:timer/state timer-state) action timer-type]
-      ;; Simple timer behavior
-      [:state/reset :click :simple]     (timer-start timer-state now-ms)
-      [:state/running :click :simple]   (let [elapsed (timer-elapsed-ms timer-state now-ms)]
-                                          (assoc timer-state
-                                                 :timer/state :state/stopped
-                                                 :timer/accumulated-ms elapsed
-                                                 :timer/session-start nil))
-      [:state/stopped :click :simple]   (timer-reset timer-state)
+  (let [state (:timer/state timer-state)
+        timer-type (:timer/type timer-state)]
+    (if (= action :click)
+      ;; Click actions with timer-type specific behavior
+      (case [state timer-type]
+        [:state/reset :simple]
+        (timer-start timer-state now-ms)
 
-      ;; Pausable timer behavior
-      [:state/reset :click :pausable]   (timer-start timer-state now-ms)
-      [:state/running :click :pausable] (timer-pause timer-state now-ms)
-      [:state/paused :click :pausable]  (timer-resume timer-state now-ms)
+        [:state/running :simple]
+        (let [elapsed (timer-elapsed-ms timer-state now-ms)]
+          (assoc timer-state
+                 :timer/state :state/stopped
+                 :timer/accumulated-ms elapsed
+                 :timer/session-start nil))
 
-      ;; Direct transitions
-      [:state/stopped :start]   (timer-start timer-state now-ms)
-      [:state/reset :start]     (timer-start timer-state now-ms)
-      [:state/running :pause]   (timer-pause timer-state now-ms)
-      [:state/paused :resume]   (timer-resume timer-state now-ms)
-      [_ :reset]                (timer-reset timer-state)
-      [_ :init]                 empty-timer-state
+        [:state/stopped :simple]
+        (timer-reset timer-state)
 
-      ;; Default: no change
-      timer-state)))
+        [:state/reset :pausable]
+        (timer-start timer-state now-ms)
+
+        [:state/running :pausable]
+        (timer-pause timer-state now-ms)
+
+        [:state/paused :pausable]
+        (timer-resume timer-state now-ms)
+
+        timer-state) ; default
+
+      ;; Direct actions - state-based, timer-type agnostic
+      (case action
+        :start (if (contains? #{:state/stopped :state/reset} state)
+                 (timer-start timer-state now-ms)
+                 timer-state)
+        :pause (if (= state :state/running)
+                 (timer-pause timer-state now-ms)
+                 timer-state)
+        :resume (if (= state :state/paused)
+                  (timer-resume timer-state now-ms)
+                  timer-state)
+        :reset (timer-reset timer-state)
+        :init empty-timer-state
+        timer-state)))) ; default
 
 (defn zero-pad
   "Add leading zero if needed"
@@ -131,20 +148,80 @@
       (timer-start 1000)
       (timer-pause 4000))
 
-  ;; Test click behavior cycle
+  ;; Test simple timer click behavior cycle
   (let [time-base 1000
-        state1 (timer-transition empty-timer-state :click time-base)
-        state2 (timer-transition state1 :click (+ time-base 2000))
-        state3 (timer-transition state2 :click (+ time-base 5000))]
-    {:first-click state1
-     :second-click state2
-     :third-click state3})
+        state1    (timer-transition empty-timer-state :click time-base)
+        state2    (timer-transition state1 :click (+ time-base 2000))
+        state3    (timer-transition state2 :click (+ time-base 5000))]
+    {:first-click  state1   ; reset -> running
+     :second-click state2  ; running -> stopped (2s elapsed)
+     :third-click  state3}) ; stopped -> reset
+
+  ;; Test pausable timer click behavior cycle
+  (let [pausable-timer (assoc empty-timer-state :timer/type :pausable)
+        time-base      1000
+        state1         (timer-transition pausable-timer :click time-base)
+        state2         (timer-transition state1 :click (+ time-base 2000))
+        state3         (timer-transition state2 :click (+ time-base 3000))]
+    {:first-click  state1   ; reset -> running
+     :second-click state2  ; running -> paused (2s accumulated)
+     :third-click  state3}) ; paused -> running (resume)
 
   ;; Test time formatting
-  (elapsed-ms->time-str 0)
-  (elapsed-ms->time-str 15000)
-  (elapsed-ms->time-str 65000)
-  (elapsed-ms->time-str 3661000)
+  (elapsed-ms->time-str 0)      ; "00:00:00"
+  (elapsed-ms->time-str 15000)  ; "00:00:15"
+  (elapsed-ms->time-str 65000)  ; "00:01:05"
+  (elapsed-ms->time-str 3661000); "01:01:01"
+
+  ;; Test timer display text (strips leading "00:")
+  (let [test-state (assoc empty-timer-state
+                          :timer/state :state/running
+                          :timer/session-start 1000)]
+    [(timer-display-text test-state 1000)    ; "00:00"
+     (timer-display-text test-state 16000)   ; "00:15"
+     (timer-display-text test-state 66000)   ; "01:05"
+     (timer-display-text test-state 3662000)]) ; "01:01:01"
+
+  ;; Test elapsed time calculation
+  (let [running-state {:timer/state          :state/running
+                       :timer/accumulated-ms 2000
+                       :timer/session-start  5000}
+        paused-state  {:timer/state          :state/paused
+                       :timer/accumulated-ms 3000
+                       :timer/session-start  nil}]
+    {:running-elapsed (timer-elapsed-ms running-state 8000) ; 2000 + (8000-5000) = 5000
+     :paused-elapsed  (timer-elapsed-ms paused-state 8000)}) ; 3000 (no additional time)
+
+  ;; Test reset preserves timer type
+  (let [pausable-timer (assoc empty-timer-state :timer/type :pausable)]
+    (:timer/type (timer-reset pausable-timer))) ; should be :pausable
+
+  ;; BUG: Direct actions don't work! (these should work but don't)
+  (let [stopped-timer {:timer/state          :state/stopped
+                       :timer/type           :simple
+                       :timer/accumulated-ms 5000
+                       :timer/session-start  nil
+                       :timer/last-display   "00:00"}
+        running-timer {:timer/state          :state/running
+                       :timer/type           :pausable
+                       :timer/accumulated-ms 0
+                       :timer/session-start  1000
+                       :timer/last-display   "00:00"}
+        paused-timer  {:timer/state          :state/paused
+                       :timer/type           :pausable
+                       :timer/accumulated-ms 2000
+                       :timer/session-start  nil
+                       :timer/last-display   "00:00"}]
+    {:start-from-stopped (timer-transition stopped-timer :start 10000)  ; BUG: should start but doesn't
+     :start-from-reset   (timer-transition empty-timer-state :start 10000) ; BUG: should start but doesn't
+     :pause-from-running (timer-transition running-timer :pause 5000)    ; BUG: should pause but doesn't
+     :resume-from-paused (timer-transition paused-timer :resume 8000)})  ; BUG: should resume but doesn't
+
+  ;; These actions DO work (wildcards)
+  {:reset-works (timer-transition {:timer/state :state/running
+                                   :timer/type  :simple} :reset 1000)
+   :init-works  (timer-transition {:timer/state :state/running
+                                   :timer/type  :simple} :init 1000)}
 
   :rcf)
 
